0f2b93be8e72833572520a2319da47a8dd7ca38d
Loup Salome <loup.salome@iut-rodez.fr>
2021-11-20 21:31:04 +0100

    reModifScriptBash

--- scriptBash/suppressionSuperfluxRdepends.sh
+++ scriptBash/suppressionSuperfluxRdepends.sh
@@ -1,12 +1,16 @@
-
 #!/bin/bash
 
-apt rdepends $1 | sed -E "s/.*://" > transition.txt
+apt rdepends $1 | sed -E "s/\(.*\)//" > transition.txt 
 
-sed "/$1/d"   transition.txt > autre.txt 
 
-sed "/^$/d" autre.txt > transition.txt 
+sed -E "s/^.*://" transition.txt > autre.txt
 
-sed "s/[[:space:]]//" transition.txt > autre.txt
+sed "/$1/d" autre.txt > transition.txt 
+
+sed "/^$/d" transition.txt > autre.txt 
+
+sed "s/[[:space:]]//" autre.txt > transition.txt 
+
+cat transition.txt > autre.txt
 
 rm transition.txt 
